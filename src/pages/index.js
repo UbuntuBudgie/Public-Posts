2e0f238a2280a216713c99d8f4042a16ea94be15
GrindamN <nstojic66@hotmail.com>
2024-01-05 18:29:37 +0100

    Updated main page.

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,7 +20,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            Ubuntu Budgie Tutorial in 5 mins (or maybe less) ⏱️
           </Link>
         </div>
       </div>
